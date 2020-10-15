68f0ccd286112969f05c44a2420f1894cf0a0200
unknown <you@example.com>
2020-10-15 12:11:17 +0600

    Add to github

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:1000,
+        margin:5,
+        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+     $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+    });
+  });
+  
